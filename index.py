1ec8ec80aa62ed2e1260efbd4252c792e2991f72
Dheeraj Sonkar <dheerajsonkarmy@gmail.com>
2025-09-23 12:21:16 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,25 @@
+print("hello world")
+data = 12
+data="anand"
+print(data)
+
+#it is concept of list
+mylist = ["Dheeraj Sonkar", "24MCA045", "MCA 2 Year" ]
+print(f" my name is {mylist[0]}\n and my roll number is\n {mylist[1]} and I am studying in {mylist[2]}")
+
+
+mydata = ("Dheeraj Sonkar", "24MCA045", "MCA", 73920012757,"dheerajsonkarmy@gmail.com")
+print(mydata[0],type(mydata[0]))
+print( mydata[1],type(mydata[1]))
+print( mydata[2],type(mydata[2]))
+print( mydata[3],type(mydata[3]))
+print( mydata[4],type(mydata[4]))
+thisset={"apple","banana","cherry","apple"}
+print(thisset)
+print(type(thisset))
+
+dict={"name":"Dheeraj Sonkar","rollno":"24MCA045","course":"MCA"}
+dict["country"]= "india"
+print(dict)
+dict.pop("country")
+print(dict )
